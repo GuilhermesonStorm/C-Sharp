3b018c0ac4e923df4e9bd81e435bd15867ccbd70
GuilhermesonStorm <67202219+GuilhermesonStorm@users.noreply.github.com>
2021-08-09 18:48:11 -0300

    Add files via upload

---
+++ ExercicioAleatorio1/ExercicioAleatorio1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExercicioAleatorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int velocidade, limite = 80 , multa;
+            Console.WriteLine("Entre com uma velocidade: ...");
+            velocidade = int.Parse(Console.ReadLine());
+            if (velocidade > 80)
+            {
+                multa = (velocidade - limite) * 5;
+                Console.WriteLine($"O valor da multa é de: R$ {multa}");
+            }
+            else if (velocidade < 0)
+            {
+                Console.WriteLine("Velocidade inválida");
+            }
+            else
+            {
+                Console.WriteLine("Veículo dentro do limite de velocidade");
+            }
+
+        }
+    }
+}
