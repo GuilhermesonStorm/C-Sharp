0ada8d4eaa1108e2038563399ec248c7b8d68b73
GuilhermesonStorm <67202219+GuilhermesonStorm@users.noreply.github.com>
2021-08-09 19:52:30 -0300

    Add files via upload

---
+++ ExercicioAleatorio4/ExercicioAleatorio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioAleatorio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Entre com um número: ...");
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Número par");
+            }
+            else
+            {
+                Console.WriteLine("Número ímpar");
+            }
+        }
+    }
+}
