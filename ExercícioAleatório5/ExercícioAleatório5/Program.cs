ac1e1701db343cc68f59e40cf40ae35f151bfbc5
GuilhermesonStorm <67202219+GuilhermesonStorm@users.noreply.github.com>
2021-08-10 17:44:46 -0300

    Add files via upload

---
+++ ExercícioAleatório5/ExercícioAleatório5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercícioAleatório5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano;
+            Console.WriteLine("Entre com um ano: ...");
+            ano = int.Parse(Console.ReadLine());
+            if (ano % 4 == 0)
+            {
+                Console.WriteLine("O ano é bissexto.");
+            }
+            else
+            {
+                Console.WriteLine("O ano não é bissexto.");
+            }
+        }
+    }
+}
