a2cf4e432f37e947b76fae9ce2a7322e21331570
GuilhermesonStorm <67202219+GuilhermesonStorm@users.noreply.github.com>
2021-08-10 17:50:24 -0300

    Add files via upload

---
+++ ExercícioAleatório6/ExercícioAleatório6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercícioAleatório6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano, idade, dif, anoAtual = DateTime.Now.Year;
+            do
+            {
+                Console.WriteLine("Entre com um ano válido.");
+                ano = int.Parse(Console.ReadLine());
+            } while (ano < 0 || ano > anoAtual);
+            idade = anoAtual - ano;
+            dif = 18 - idade;
+
+            if (dif > 0)
+            {
+                Console.WriteLine($"Falta {dif} ano(s) para o alistamento.");
+            }
+            else if (dif < 0)
+            {
+                Console.WriteLine($"Passou-se {dif * -1} ano(s) do alistamento.");
+            }
+            
+            
+        }
+    }
+}
